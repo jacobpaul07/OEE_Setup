9bed384983c5d85cfc04be5f9d5867791efff3c5
Jacob Paul <p.jacobpaul07@gmail.com>
2022-01-25 14:40:28 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-sudo apt-get install -y \
-    apt-transport-https \
+sudo apt update
+sudo apt install python3-pip
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+
+sudo apt-get install \
     ca-certificates \
     curl \
-    software-properties-common 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main"
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
 sudo apt-get update
-sudo apt-get -y install docker-engine 
-# add current user to docker group so there is no need to use sudo when running docker
-sudo usermod -aG docker $(whoami)
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo pip install docker-compose
