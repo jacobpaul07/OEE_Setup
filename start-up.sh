1bcbaa278880bf7995a86aa68226f92df49ed348
Jacob Paul <p.jacobpaul07@gmail.com>
2022-01-25 15:03:48 +0530

    Update start-up.sh

--- start-up.sh
+++ start-up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # install docker
-./docker-install.sh
+sudo ./docker-install.sh
 # Deploy the docker images
-docker-compose -f ./Kafka/docker-compose.yml up -d --force-recreate
-docker-compose -f ./Kafka/docker-compose.yml up -d --force-recreate
-docker-compose -f ./OEE-BE/docker-compose.yml up -d --force-recreate
-docker-compose -f ./OEE-UI/docker-compose.yml up -d --force-recreate
-docker-compose -f ./OPC-Server/docker-compose.yml up -d --force-recreate
+sudo docker-compose -f ./Kafka/docker-compose.yml up -d --force-recreate
+sudo docker-compose -f ./Kafka/docker-compose.yml up -d --force-recreate
+sudo docker-compose -f ./OEE-BE/docker-compose.yml up -d --force-recreate
+sudo docker-compose -f ./OEE-UI/docker-compose.yml up -d --force-recreate
+sudo docker-compose -f ./OPC-Server/docker-compose.yml up -d --force-recreate
