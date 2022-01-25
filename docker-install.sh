c661f8c2ce0d4cbdb50e37acbb462ca2216ec448
Jacob Paul <p.jacobpaul07@gmail.com>
2022-01-25 15:18:44 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -3,21 +3,40 @@ sudo apt update
 sudo apt install python3-pip
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
+    apt-transport-https \
     ca-certificates \
     curl \
-    gnupg \
-    lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
-
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce
+sudo docker run hello-world
+# Linux post-install
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+# sudo apt-get update
+
+# sudo apt-get install \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
+
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo pip install docker-compose
