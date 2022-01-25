de6d8ce3584476d1bf0f626950ff91fc4b841d57
Jacob Paul <p.jacobpaul07@gmail.com>
2022-01-25 15:48:19 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get install -y \
-    apt-transport-https \
+
+sudo apt-get update
+
+sudo apt-get install \
     ca-certificates \
     curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # Linux post-install
 sudo groupadd docker
@@ -22,5 +26,4 @@ sudo systemctl enable docker
 # docker-compose install
 sudo curl -L --fail https://github.com/docker/compose/releases/download/1.29.2/run.sh -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
 
