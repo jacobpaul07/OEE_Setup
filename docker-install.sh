416c6ed6571b544daf904ad91a230965cd5497eb
Jacob Paul <p.jacobpaul07@gmail.com>
2022-01-25 15:59:15 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -3,13 +3,13 @@ sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
 sudo apt-get update -y
 
-sudo apt-get install -y \
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
